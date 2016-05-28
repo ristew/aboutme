2cdb1454913697d54ed13ddad60566da13f66b84
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2016-05-28 12:04:03 -0500

    add about, contact

--- aboutme.rb
+++ aboutme.rb
@@ -17,6 +17,11 @@ module AboutMe::Controllers
             render :index
         end
     end
+    class About < R '/about'
+        def get
+            render :about
+        end
+    end
 end
 
 module AboutMe::Views
@@ -36,12 +41,33 @@ module AboutMe::Views
         h3 "use w, a, s, d to navigate"
         a "home (w)", :href => '#', :id => "wlink", :class => "selector"
         br
-        a "about (a)", :href => '#a', :id => "alink", :class => "selector"
+        a "about (a)", :href => '/about', :id => "alink", :class => "selector"
         a "github (d)", :href => 'https://github.com/ristew', :id => "dlink", :class => "selector"
         br
-        a "contact (s)", :href => '#s', :id => "slink", :class => "selector"
+        a "contact (s)", :href => 'mailto:riley.a.stewart@vanderbilt.edu', :id => "slink", :class => "selector"
     end
-end
 
-def AboutMe.create
+    def about
+        h3 "who I am"
+        li "Riley Stewart"
+        li "rising senior at vanderbilt"
+        li "hacker and inventor"
+        h3 "the things I enjoy:"
+        li "coffee"
+        li "python"
+        li "ruby (this site is built with it)"
+        li "rust"
+        li "REST"
+        li "javascript"
+        li "making something work"
+        li "making something work well"
+        h3 "what I've done"
+        li "a RESTy api in j2ee (ugh)"
+        li "a song generator"
+        li "too many brainf*ck interpreters"
+        li do
+            span "everything else at "
+            a "my github", :href => "https://github.com/ristew"
+        end
+    end
 end
